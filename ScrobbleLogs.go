77718c9735f9e2d99c11fb55ce7e5d04dea4f83d
Jason Stallings <jason@stallin.gs>
2015-01-14 02:23:17 -0600

    CODE

---
+++ ScrobbleLogs.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	session := os.Args[1]
+	LastLines := strings.Split(os.Args[2], "\n")
+	
+	for index, each := range LastLines {
+		var _ = index
+        //fmt.Printf("Divine value [%d] is [%s]\n", index, each)
+		LastString := strings.Split(each, "]: ")[1]
+		LastArray := strings.Split(LastString, "|")
+		artist := LastArray[1]
+		track := LastArray[2]
+		timestamp := LastArray[4]
+	
+		fmt.Println(artist + " " + track + " " + timestamp)
+		scrobble(session, artist, track, timestamp)
+    }
+}
+
+func scrobble(session, artist, track, timestamp string) {
+	cmd := exec.Command("/usr/bin/perl", "lfmCMD.pl", "method=track.scrobble", "artist=" + artist, "track=" + track, "timestamp=" + timestamp, "sk=" + session)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	var _ = err
+	fmt.Printf("%s", out.String())
+}
